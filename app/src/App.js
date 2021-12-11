135d861d9579291e7fa0228bbaf3b7b7ce8cd6a9
Creeland <cree@provinsal.com>
2021-12-10 20:05:16 -0600

    remove unusued constants

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,7 @@
 import React, {useEffect, useState } from 'react';
 import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
 import CandyMachine from './CandyMachine'
 import Arcade from './assets/arcade.js'
-
-// Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null)
